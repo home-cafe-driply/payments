fa9ec4ea5d8189e3eb45ec07dc4e92e7838f1183
havegrit <shin_jaean@naver.com>
2025-02-24 18:33:37 +0900

    project init

---
+++ src/test/java/com/driply/payments/PaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.driply.payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
