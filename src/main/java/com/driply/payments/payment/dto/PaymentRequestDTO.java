e6a000b09b8a2e787a9b9a7e63e63c8c86cef0b3
havegrit <shin_jaean@naver.com>
2025-03-15 15:09:13 +0900

    feat: add payment request dto

---
+++ src/main/java/com/driply/payments/payment/dto/PaymentRequestDTO.java
@@ -0,0 +1,20 @@
+package com.driply.payments.payment.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentRequestDTO {
+    private Long paymentId;
+
+    private Long orderId;
+
+    private String paymentProvider;
+
+    private Double amount;
+}
