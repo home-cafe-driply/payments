da6aa0f52ebb5a4523e9aca5f893cc0cef5180be
havegrit <shin_jaean@naver.com>
2025-04-15 14:20:44 +0900

    chore: 자잘한 수정 작업

--- src/main/java/com/driply/payments/payment/controller/PaymentController.java
+++ src/main/java/com/driply/payments/payment/controller/PaymentController.java
@@ -13,25 +13,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
 import java.util.Map;
 
 @Controller
+//@RestController
 //@RequestMapping("/api/v1/payment")
 @RequiredArgsConstructor
 public class PaymentController {
     private final PaymentService paymentService;
 
-
     /**
      * 위젯 결제와 일반결제 요청을 처리합니다.
-     * @param request 결제 요청 정보를 전달 받습니다.
+     * @param request 결제 요청에 관한 메타 데이터를 전달 받습니다.
      * @param requestDTO 결제사별로 api 요청에 필요한 데이터를 담고 있습니다.
      * @return 결제사의 응답 결과를 바탕으로 200(결제 승인 완료) 혹은 400(결제 승인 실패) status code를 포함한 응답을 반화합니다.
-     * @throws IOException 예외 처리 구현해야 합니다. 추후 수정 예정.
      */
     @PostMapping(value = {"/confirm/widget", "/confirm/payment"})
     public ResponseEntity<Map<String, Object>> confirmPayment(HttpServletRequest request, @RequestBody PaymentRequestDTO requestDTO) throws IOException {
@@ -45,7 +43,7 @@ public class PaymentController {
      * @return 정기결제 성공 여부에 따라 200(정기결제 성공) 혹은 400(정기결제 실패) status code를 포함한 응답을 반환합니다.
      * @throws IOException
      */
-    @RequestMapping(value = "/confirm-billing")
+    @PostMapping(value = "/confirm-billing")
     public ResponseEntity<Map<String, Object>> confirmBilling(@RequestBody BillingRequestDTO requestDTO) throws IOException {
         Map<String, Object> response = paymentService.confirmBilling(requestDTO);
         return ResponseEntity.status(response.containsKey("error") ? 400 : 200).body(response);
@@ -57,7 +55,7 @@ public class PaymentController {
      * @return 빌링키 발급 성공 여부에 따라 200(빌링키 발급 성공) 혹은 400(빌링키 발급 실패) status code를 포함한 응답을 반환합니다.
      * @throws IOException
      */
-    @RequestMapping(value = "/issue-billing-key")
+    @PostMapping(value = "/issue-billing-key")
     public ResponseEntity<Map<String, Object>> issueBillingKey(@RequestBody BillingKeyRequestDTO requestDTO) throws IOException {
         Map<String, Object> response = paymentService.issueBillingKey(requestDTO);
         return ResponseEntity.status(response.containsKey("error") ? 400 : 200).body(response);
