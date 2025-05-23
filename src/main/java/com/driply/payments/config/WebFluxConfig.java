9d24e6df011a3000629a1d97a20ca1aceca285cc
havegrit <shin_jaean@naver.com>
2025-05-23 16:10:07 +0900

    feat: add cors mapping

--- src/main/java/com/driply/payments/config/WebFluxConfig.java
+++ src/main/java/com/driply/payments/config/WebFluxConfig.java
@@ -8,9 +8,12 @@ import org.springframework.web.reactive.config.WebFluxConfigurer;
 public class WebFluxConfig implements WebFluxConfigurer {
 
     @Override
-    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/templates/", "classpath:/static/")
-                .setCacheControl(CacheControl.maxAge(10, TimeUnit.MINUTES));
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+            .allowedOrigins("http://localhost:8080")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("Authorization", "Content-Type")
+            .allowCredentials(true)
+            .maxAge(3600);
     }
 }
