ed438b7f19bbb0984c702e95b5f669ffa3ba28be
havegrit <shin_jaean@naver.com>
2025-06-18 00:26:21 +0900

    chore: 들여쓰기 및 불필요한 코드 제거

--- src/main/java/com/driply/payments/payment/gateway/PaymentGateway.java
+++ src/main/java/com/driply/payments/payment/gateway/PaymentGateway.java
@@ -18,12 +18,12 @@ public interface PaymentGateway {
 	// 결제 상태 조회
 	PaymentStatus checkStatus(String transactionId);
 
-    // 결제 취소
-    boolean refundPayment(String transactionId);
+	// 결제 취소
+	boolean refundPayment(String transactionId);
 
-    // 결제 수단 정보
-    String getPGType();
+	// 결제 수단 정보
+	String getPGType();
 
-    // 에러 정보
-    PaymentError getLastError();
+	// 에러 정보
+	PaymentError getLastError();
 }
