39c07104a687279cd31a84e04762ba2b3adc5e5a
havegrit <shin_jaean@naver.com>
2025-07-01 00:27:59 +0900

    fix: chanage allowedOrigins

--- src/main/java/com/driply/payments/config/WebFluxConfig.java
+++ src/main/java/com/driply/payments/config/WebFluxConfig.java
@@ -10,23 +10,23 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebFluxConfig implements WebFluxConfigurer {
 
-    @Value("${toss.payments.base-url}")
-    public String tossBaseUrl;
+	@Value("${toss.payments.base-url}")
+	public String tossBaseUrl;
 
-    @Override
-    public void addCorsMappings(CorsRegistry corsRegistry) {
-        corsRegistry.addMapping("/**")
-            .allowedOrigins("http://localhost:8080")
-            .allowedMethods("GET", "POST", "PUT", "DELETE")
-            .allowedHeaders("Authorization", "Content-Type")
-            .allowCredentials(true)
-            .maxAge(3600);
-    }
+	@Override
+	public void addCorsMappings(CorsRegistry corsRegistry) {
+		corsRegistry.addMapping("/**")
+			.allowedOrigins("https://driply-coffee.shop")
+			.allowedMethods("GET", "POST", "PUT", "DELETE")
+			.allowedHeaders("Authorization", "Content-Type")
+			.allowCredentials(true)
+			.maxAge(3600);
+	}
 
-    @Bean
-    public WebClient tossWebClient() {
-        return WebClient.builder()
-            .baseUrl(tossBaseUrl)
-            .build();
-    }
+	@Bean
+	public WebClient tossWebClient() {
+		return WebClient.builder()
+			.baseUrl(tossBaseUrl)
+			.build();
+	}
 }
